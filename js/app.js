661e222dc053b97b3bfae3664ddff9b1dfdea354
Patrick <cayerp@gmail.com>
2019-11-08 12:58:17 -0500

    Js adjustments
    added active nav states when scrolling

--- js/app.js
+++ js/app.js
@@ -6,11 +6,33 @@ const myList = document.getElementById('myList');
 // new list item
 
 let newListItem = document.createElement('li');
-newListItem.innerHTML = `<a href="#section1">Section1</a>
-<a href="#section2">Section2</a>
-<a href="#section3">Section3</a>`;
+newListItem.innerHTML = `<a href="#section1">Section 1</a>
+<a href="#section2">Section 2</a>
+<a href="#section3">Section 3</a>
+<a href="#section4">Section 4</a>`;
 
 myList.appendChild(newListItem);
+
+
+// ------------ Adds new section to DOM -------------
+
+
+const newSec = document.getElementById('sections');
+// new section item
+
+let newSecItem = document.createElement('section3');
+newSecItem.innerHTML = `
+<section id="section4" data-nav="Section 4" class="your-active-class">
+      <div class="landing__container">
+        <h2>Section 4</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+      </div>
+    </section>
+`;
+
+sections.appendChild(newSecItem);
 
 
 // ------------responsive Navigation on smaller screens -----------
@@ -38,7 +60,7 @@ function navbarLinkClick() {
 // script to create smooth scrolling for navigation
 
 // *********** window.scrollInToView()**************
-function smoothScroll(event) {
+smoothScroll = (event) => {
     event.preventDefault();
     const targetId = event.currentTarget.getAttribute("href")==="#" ? "body" : event.currentTarget.getAttribute("href");
     document.querySelector(targetId).scrollIntoView({
@@ -49,13 +71,32 @@ function smoothScroll(event) {
 
 
 
+/// script for active navigation style scroll
+
+window.addEventListener('scroll', event => {
+    let nav = document.querySelector('.navbar__menu');
+    
+    (window.scrollY >= 1) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+});
 
 
-
-
-
-
-
+window.addEventListener('scroll', event => {
+    let navLinks = document.querySelectorAll('nav ul li a');
+    let fromTop = window.scrollY;
+    
+    navLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+        
+        if (
+            section.offsetTop <= fromTop && 
+            section.offsetTop + section.offsetHeight > fromTop
+        ) {
+            link.classList.add('current');
+        } else {
+            link.classList.remove('current');
+        }
+    });
+});
 
 
 
